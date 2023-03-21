f380a3242ee530c3ee50d08f313598776089d7ec
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 08:17:11 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // Skapa en klass Personnummer som lagrar ett svenska personnummer XXXXXX-XXXX
+        //   - Validera att det är korrekt i konstruktorn
+        //   - Lägg till toString
+        //   - Lägg till isMan och isKvinna
+        // Exempel: 561231-4913
+
+
+    }
+}
