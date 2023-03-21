295a9682d3dae1511b62e4d72336d9ea94058a8a
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-21 09:12:34 +0100

    Corrected spelling mistakes

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        // Skapa en klass Personnummer som lagrar ett svenska personnummer XXXXXX-XXXX
+        // Skapa en klass Personnummer som lagrar ett svenskt personnummer XXXXXX-XXXX
         //   - Validera att det är korrekt i konstruktorn
         //   - Lägg till toString
         //   - Lägg till isMan och isKvinna
